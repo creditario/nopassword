5ab8abe9fd9b57c489789f9b2b97fa6b067c1489
juan-bojorges <87720478+juan-bojorges@users.noreply.github.com>
2021-12-03 11:06:06 -0600

    Añadir modelo Session (#3)

---
+++ db/migrate/20211202211706_create_no_password_sessions.rb
@@ -0,0 +1,16 @@
+class CreateNoPasswordSessions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :no_password_sessions do |t|
+
+      t.timestamp :expires_at
+      t.timestamp :claimed_at
+      t.string :token, null: false
+      t.string :user_agent, null: false
+      t.string :remote_addr, null: false
+      t.string :return_url
+      t.string :email, null: false
+
+      t.timestamps
+    end
+  end
+end
---
+++ test/dummy/db/schema.rb
@@ -0,0 +1,27 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_12_02_211706) do
+
+  create_table "no_password_sessions", force: :cascade do |t|
+    t.datetime "expires_at", precision: 6
+    t.datetime "claimed_at", precision: 6
+    t.string "token", null: false
+    t.string "user_agent", null: false
+    t.string "remote_addr", null: false
+    t.string "return_url"
+    t.string "email", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
