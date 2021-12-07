83e3cf7eade6d366f5b22968be0cf881cb26e1b2
armando escalier <armando.escalier@michelada.io>
2021-12-07 14:58:43 -0600

    Se añade array para idiomas
    Se añaden :en y :es como idiomas disponibles

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -19,7 +19,7 @@ module Dummy
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    config.i18n.available_locales = :es
+    config.i18n.available_locales = [:en, :es]
     config.i18n.default_locale = :es
   end
 end
