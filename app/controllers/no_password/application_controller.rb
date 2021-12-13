9145e5f1c61c26d1cc4b20be3798298fc58fa663
armando escalier <armando.escalier@michelada.io>
2021-12-12 21:18:08 -0600

    Cambios requeridos en code review
    El macro 'helper_method' fue removido de ApplicationController y reubicado en el concern ControllerHelpers, para permitir el uso de sus métodos como métodos de instancia se incluye el método 'included'
    
    Se modificó el nombre del primer párametro requerido del método 'sign_in' por 'session_model'
    
    Se hizo refactorización del código en ControllerHelpers para comprobar valores de la sesión, creando los métodos 'claimed?' y 'expired?' en el modelo Session

--- app/controllers/no_password/application_controller.rb
+++ app/controllers/no_password/application_controller.rb
@@ -1,5 +1,4 @@
 module NoPassword
   class ApplicationController < ActionController::Base
-    helper_method :current_session, :signed_in_session?, :sign_in, :sign_out
   end
 end
--- app/controllers/no_password/concerns/controller_helpers.rb
+++ app/controllers/no_password/concerns/controller_helpers.rb
@@ -8,39 +8,41 @@ module NoPassword
     module ControllerHelpers
       extend ActiveSupport::Concern
 
-      def current_session
-        session_id = session[session_key]
-        @session ||= NoPassword::Session.find_by_id(session_id)
-      end
-
-      def signed_in_session?
-        current_session.present?
-      end
-
-      def sign_in(input_session, key = nil, data = nil)
-        current_time = Time.zone.now
-
-        if input_session.claimed_at && input_session.expires_at > current_time
-          session[session_key] = input_session.id
-
-          if key.present? && data.present?
-            session[session_key(key)] = data
-          end
-          input_session
+      included do
+        def current_session
+          session_id = session[session_key]
+          @session ||= NoPassword::Session.find_by_id(session_id)
         end
-      end
 
-      def sign_out(key = nil)
-        session.delete(session_key)
-        session.delete(session_key(key)) if key.present?
-        @session = nil
-        true
-      end
+        def signed_in_session?
+          current_session.present?
+        end
 
-      protected
+        def sign_in(session_model, key = nil, data = nil)
+          if session_model.claimed? && session_model.expired?
+            session[session_key] = session_model.id
 
-      def session_key(value = "id")
-        "—-no_password_session_#{value}"
+            if key.present? && data.present?
+              session[session_key(key)] = data
+            end
+            session_model
+          end
+        end
+
+        def sign_out(key = nil)
+          session.delete(session_key)
+          session.delete(session_key(key)) if key.present?
+          @session = nil
+          true
+        end
+
+        helper_method :current_session, :signed_in_session?, :sign_in, :sign_out
+
+        protected
+
+        def session_key(value = "id")
+          "—-no_password_session_#{value}"
+        end
       end
     end
   end
