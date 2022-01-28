1f20abd9f84a966decf1d116e43471a8db2ea699
Juan Bojorges <juan.bojorges@michelada.io>
2022-01-28 14:29:17 -0600

    Actualizar manifest en dummy
    - Se actualizó manifest en dummy para añadir //= link application.js para resolver errores de Test en el CI de github.

--- test/dummy/app/assets/config/manifest.js
+++ test/dummy/app/assets/config/manifest.js
@@ -1,6 +1,7 @@
 //= link_tree ../images
 //= link_tree ../../javascript/controllers .js
 //= link application.css
+//= link application.js
 
 //= link_tree ../../../vendor/javascript .js
 //= link_tree ../builds
