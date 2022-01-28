7b6f58f2ac4faf759e04dc1af4fc19aa58ecf9dd
juan-bojorges <87720478+juan-bojorges@users.noreply.github.com>
2022-01-28 16:53:21 -0600

    Chore/Actualizar git ignore y manifest (#22)

--- test/dummy/app/assets/config/manifest.js
+++ test/dummy/app/assets/config/manifest.js
@@ -1,6 +1,7 @@
 //= link_tree ../images
-//= link_directory ../stylesheets .css
+//= link_tree ../../javascript/controllers .js
+//= link application.css
+//= link application.js
 
-//= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link_tree ../builds
