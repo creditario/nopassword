9c351242ab01b9ecd2f6e92c22eecdabff6dde8b
Juan Bojorges <juan.bojorges@michelada.io>
2022-03-03 14:22:40 -0600

    Añadir hooks en SessionConfirmationController
    - Se añadieron hooks en SessionConfirmationController para manejar redireccionamientos en edit y update.
    - Se cambió provisionalmente el método de sign_in para evitar conflictos con el método de sign_in en devise.

--- app/controllers/no_password/session_confirmations_controller.rb
+++ app/controllers/no_password/session_confirmations_controller.rb
@@ -10,22 +10,31 @@ module NoPassword
         friendly_token = verify_token(params[:token])
 
         current_session = find_and_validate_token(friendly_token)
-        return sign_in_session(current_session) if current_session.present?
+
+        if !current_session.present? && respond_to?(:custom_redirect)
+          custom_redirect
+        elsif current_session.present?
+          sign_in_session(current_session)
+        end
       end
     end
 
     def update
       current_session = find_and_validate_token(params[:token])
 
-      return sign_in_session(current_session) if current_session.present?
-
-      response.status = :unprocessable_entity
-      render turbo_stream: turbo_stream.update("notifications", partial: "notification")
+      if !current_session.present? && respond_to?(:custom_redirect)
+        custom_redirect
+      elsif current_session.present?
+        sign_in_session(current_session)
+      else
+        response.status = :unprocessable_entity
+        render turbo_stream: turbo_stream.update("notifications", partial: "notification")
+      end
     end
 
     private
 
-    def sign_in(session_model, key = nil, data = nil)
+    def nopassword_sign_in(session_model, key = nil, data = nil)
       if session_model.claimed? && !session_model.expired?
         session[session_key] = session_model.id
 
@@ -38,9 +47,13 @@ module NoPassword
 
     def sign_in_session(session)
       claimed_session = SessionManager.new.claim(session.token, session.email)
-      sign_in(claimed_session)
+      nopassword_sign_in(claimed_session)
 
-      redirect_to session.return_url || main_app.root_path
+      if respond_to?(:force_device_login)
+        force_device_login
+      else
+        redirect_to session.return_url || main_app.root_path
+      end
     end
 
     def find_and_validate_token(friendly_token)
