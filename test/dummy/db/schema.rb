e9d161f87654b9494756d37e25feb34f927825ef
Mario Alberto Chávez <mario.chavez@gmail.com>
2022-10-25 10:56:56 -0500

    Actualiza dependencias v0.1.1

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2022_01_25_215037) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,5 +25,4 @@ ActiveRecord::Schema.define(version: 2022_01_25_215037) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
 end
